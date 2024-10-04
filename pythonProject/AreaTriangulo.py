fc862c3a069566e0ce612e6420a326f439b8a3f8
KENNY DAJHANA URRESTY LOPEZ <1088947228@fup.edu>
2024-10-03 19:01:39 -0500

    Se crea el projecto

---
+++ pythonProject/AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Ejemplo para calcular el area del triangulo
+
+#entradas
+base = int (input("Ingrese la base del triangul"))
+altura = int(input)("Ingrese la altura del triangulo"))
+
+
+#proceso
+def calcularAreaTriangulo(b,a):
